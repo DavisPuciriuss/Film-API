b47b976cdda923fead91d2e37e3d45dbec19f94d
Davis Puciriuss <davis.puciriuss@kalmars.lv>
2023-08-29 16:00:16 +0300

    JSON Response Middleware for headers.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,9 +30,10 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\JsonResponse::class,
         ],
     ];
 
---
+++ app/Http/Middleware/JsonResponse.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class JsonResponse
+{
+    /**
+     * The Response Factory our app uses
+     *
+     * @var ResponseFactory
+     */
+    protected $factory;
+
+    /**
+     * JsonMiddleware constructor.
+     */
+    public function __construct(ResponseFactory $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $request->headers->set('Accept', 'application/json');
+
+        $response = $next($request);
+
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+}
